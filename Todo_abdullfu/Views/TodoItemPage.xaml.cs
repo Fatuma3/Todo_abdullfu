4e7bfa22d8316015b01a9f2398b0e8a720ab9b7a
FA33 <144830259+Fatuma3@users.noreply.github.com>
2024-03-11 00:04:06 -0400

    Add project files.

---
+++ Todo_abdullfu/Views/TodoItemPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo_abdullfu.Data;
+using Todo_abdullfu.Models;
+using Todo_abdullfu.Views;
+using Todo_abdullfu;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Todo_abdullfu.Views
+{
+    public partial class TodoListPage : ContentPage
+    {
+        public TodoListPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            
+            listView.ItemsSource = await App.Database.GetItemsAsync();
+        }
+
+        async void OnItemAdded(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TodoItemPage
+            {
+                BindingContext = new TodoItem()
+            });
+        }
+
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new TodoItemPage
+                {
+                    BindingContext = e.SelectedItem as TodoItem
+                });
+            }
+        }
+    }
+}
